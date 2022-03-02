a8219efac1c9430e60ded3c4a7612e8acdb555b3
joaocarlos-losfe <joaocarlos.losfe@gmail.com>
2022-03-02 16:45:53 -0300

    ajuste no projeto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import { Test } from './teste'
 
 function App(a,b, c) {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-
-      <Test/>
 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import Test from './teste'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Test/>
-    <App />
-    <App />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
