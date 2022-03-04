d5572ad6eda81c37a636d5e20202521b27df297e
joaocarlos-losfe <joaocarlos.losfe@gmail.com>
2022-03-04 14:31:15 -0500

    inserção dos url de origigem nos posts, obter todos os post e  obter posts por nome de usuario

--- Backend/src/routes/post_routes.js
+++ Backend/src/routes/post_routes.js
@@ -2,21 +2,51 @@ const router = require('express').Router();
 const PostModel = require('../models/post');
 const { getCurrentTime } = require('../utils/formated_datetime');
 
+router.get("/by_user/:user_name", async (req, res) => 
+{
+    try
+    {
+        const posters_by_user = await PostModel.find({username: req.params.user_name});
+
+        if(posters_by_user)
+            res.status(200).json({data: posters_by_user});
+        
+    }catch(err)
+    {
+        console.log(err);
+        res.status(500).json(err);
+    }
+}); 
+
+router.get("/", async (req, res) => 
+{
+    try
+    {
+        const posters = await PostModel.find({});
+        res.status(200).json({data: posters});
+
+    }catch(err)
+    {
+        console.log(err);
+        res.status(500).json(err);
+    }
+}); 
+
 router.post("/add", async (req, res) =>
 {
     try
     {
-        if(req.body.username && req.body.category && req.body.tags && req.body.title && req.body.description)
+        if(req.body.username && req.body.category && req.body.tags && req.body.title && req.body.description && req.body.source_url)
         {
             //verificar antes se ja existe um post com mesmo link ou titulo ou texto iguais. 
             //Deve-se rejeitar a operação
             
-            let {username, category, tags, title, description} = req.body;
+            let {username, category, tags, title, description, source_url} = req.body;
 
             tags = tags.split(';');
 
             const post_date = getCurrentTime();
-            const post = { username, post_date, category, tags, title, description } //validar antes
+            const post = { username, post_date, category, tags, title, description, source_url} //validar antes
 
             await PostModel.create(post);
             res.status(201).json({menssage: "post inserido com sucesso"});
@@ -24,9 +54,9 @@ router.post("/add", async (req, res) =>
         else
             res.status(422).json({menssage: "alguns campos estão faltando ou invalidos !"});
 
-    }catch(e)
+    }catch(err)
     {
-        console.log(e);
+        console.log(err);
         res.status(500).json(err);
     }
     
