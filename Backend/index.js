d6e79ff9a1fc0f04dc70a5f0196015911cac7e22
joaocarlos-losfe <joaocarlos.losfe@gmail.com>
2022-03-12 15:59:38 -0500

    alteraçoes

--- Backend/index.js
+++ Backend/index.js
@@ -34,6 +34,6 @@ mongoose.connect(
 });
 
 //iniciar execução do servidor
-app.listen(3000, ()=>{console.log("server is running on port 3000 ...")});
+app.listen(5000, ()=>{console.log("server is running on port 5000 ...")});
 
 
--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -3,15 +3,15 @@ import { Logo } from "./Logo";
 import { Links } from "./NavLinks";
 import { SearchBar } from "./SearchBar";
 
+import './style.css';
+
 export function NavBar() {
     return(
-        
         <nav>
             <h2>Navbar</h2>
+            <Links/>
             <Logo/>
             <SearchBar/>
-            <Links/>
         </nav>
-        
     ) 
 }
