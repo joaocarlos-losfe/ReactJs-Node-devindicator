0046593f51ef3730d657e7138987f8f1999a8187
joaocarlos-losfe <joaocarlos.losfe@gmail.com>
2022-03-23 18:00:22 -0300

    captura do campo de filtro e texto finalizado

--- frontend/src/components/SearchBar/index.jsx
+++ frontend/src/components/SearchBar/index.jsx
@@ -1,22 +1,48 @@
-import { Filter } from '../Filter';
+import { useState } from 'react';
+import {FaSistrix} from 'react-icons/fa'
+import { useFetch } from "../../hooks/useFetch";
+import { Loading } from "../Loading";
 import './style.css';
 
-import {FaSistrix} from 'react-icons/fa'
-import { useState } from 'react';
-
-
-export function SearchBar()
+export function SearchBar( { onSubmit } )
 {
-    const [searchInput, setSearchInput] = useState("");
+    const {data, isLoading} = useFetch("data-resouces/categories");
+
+    const [searchValue, setSeachValue] = useState("");
+    const [filterValue, setFilterValue] = useState("todos");
 
     return(
         
         <div className="SearchBar">
-            <form>
-                <Filter/>
+            <form> 
+                <div className="Filter">
+                    {isLoading ? <Loading/> : 
+                        <label>
+                            filtro
+                            <select className="Filters" value={filterValue} onChange={e => setFilterValue(e.target.value)}>
+                            {
+                                data.categories.map((item) =>
+                                {
+                                    return <option key={item}>{item}</option>  
+                                })
+                            }
+                            </select>
+                        </label>
+                    }
+                </div>
+
                 <div className='search'>
-                    <input type="text" maxLength="30" name='search_input' value={searchInput} placeholder='Digite para pesquisar' onChange={e=>setSearchInput(e.target.value)} />
-                    <button name='sendSearchText'> 
+                    <input 
+                        type="text" 
+                        maxLength="30" 
+                        name='search_input' 
+                        id="search_input"
+                        value={ searchValue }
+                        placeholder='Digite para pesquisar' 
+                        onChange={ e => setSeachValue(e.target.value)}
+                    />
+
+                    <button type='button' onClick={ () => onSubmit({searchValue, filterValue})}> 
                         <FaSistrix id='search_icon'/>
                     </button>
                 </div>
--- frontend/src/pages/Posts/index.jsx
+++ frontend/src/pages/Posts/index.jsx
@@ -1,5 +1,4 @@
 import { settings } from "../../configs/settings"
-import { useFetch } from "../../hooks/useFetch"
 import { Card } from "../../components/Card"
 import { Loading } from "../../components/Loading"
 import "./style.css"
@@ -11,21 +10,12 @@ import axios from "axios"
 
 export function Posts()
 {
-    const [swapPage, setSwapPage] = useState(1);
-    let pageNumber = swapPage;
+    const [swapPage, setSwapPage] = useState(1); let pageNumber = swapPage;
     const [data, setPost] = useState(null);
     const [isLoading, setLoading] = useState(true)
-
-    useEffect(() => 
-    {
-        axios.get(`${settings.localhost}/post/${pageNumber}`)
-        .then((response) => 
-        {
-            setPost(response.data);
-        }).finally(()=> setLoading(false));
-
-    }, []);
-
+    const [searchText, setSearchText] = useState("");
+    const [filterText, setfilterText] = useState("todos");
+    
     function reloadPosts()
     {
         setLoading(true);
@@ -34,39 +24,44 @@ export function Posts()
             setPost(response.data);
         }).finally(()=> setLoading(false));
     }
-    
+
+    useEffect(() => reloadPosts(), []);
+
     function decrementPage()
     {
-        if(swapPage == 1)
-            setSwapPage(swapPage)
-        else
+        if(swapPage > 1)
         {
-            setSwapPage(swapPage - 1)
-            pageNumber = swapPage - 1
-            console.log(pageNumber)
-            reloadPosts()
+            setSwapPage(swapPage - 1);
+            pageNumber = swapPage - 1;
+            reloadPosts();
         }
-            
     }
 
     function incrementPage()
     {
-        if(swapPage == data.totalpages)
-            setSwapPage(swapPage)
-        else
+        if(swapPage != data.totalpages)
         {
             setSwapPage(swapPage + 1)
             pageNumber = swapPage + 1
-            console.log(pageNumber)
-            reloadPosts()   
+            reloadPosts();
         }
+    }
+
+    const handleSubmit = ({searchValue, filterValue}) =>
+    {
+        setSearchText(searchValue);
+        setfilterText(filterValue);
+
+        console.log({searchText, filterText});
     }
 
     return(
         <div className="Posts">
             <div className="SearchArea">
-                        <SearchBar/>
-            </div>
+                <SearchBar 
+                onSubmit={handleSubmit}
+                />
+                </div>
             {
                 isLoading ? <Loading/> :
                 <>
