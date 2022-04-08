f05b04a08e5a65140de10d5d47155ed22c64a7d8
joaocarlos-losfe <joaocarlos.losfe@gmail.com>
2022-04-08 10:50:30 -0300

    pagina de login com submissão da conta parcialmente finalizada. resta fazer o envio com o axios

--- frontend/src/pages/CreateAccount/index.jsx
+++ frontend/src/pages/CreateAccount/index.jsx
@@ -1,26 +1,81 @@
 import "./style.css"
 
 import {useNavigate} from "react-router-dom"
+import { useState } from "react"
+
+import {Popup} from "../../components/Popup";
+import {SimpleMessage} from "../../components/SimpleMessage";
+import { Loading } from "../../components/Loading";
+
+const customMessages = {
+    title: {
+        valid: "Conta criada",
+        invalid: "Erro ao criar a conta !"
+    },
+
+    description:{
+        valid: "Conta criada com sucesso. Proceda a pagina de login para acessar sua conta",
+        invalid: "Verifique se todos os campos estão preenchidos ou se são validos !"
+    }
+}
 
 export const CreateAccount = ()=>{
 
     const navigate = useNavigate()
+
+    const [userName, setUserName] = useState("")
+    const [email, setEmail] = useState("")
+    const [pass, setPass] = useState("")
+
+    const [isLoading, setLoading] = useState(true)
+    const [isOpen, setIsOpen] = useState(false);
+
+    const [validAccount, setValidAccount] = useState(true)
+
+    const togglePopup = () => {
+        setIsOpen(!isOpen);
+    }
+
+    const sendAccount = async (apiUrl) => {
+
+    }
+
+    const handleSubmit = ()=>{
+
+        console.log({userName, email, pass}) // validar conta antes
+        togglePopup()
+        setValidAccount(true)
+        setLoading(false)
+
+    }
 
     return(
         <div className="CreateAccount">
             <form>
                 <h1>Criar conta</h1>
 
-                <input type="text" placeholder="Nome de usuário"/>
-                <input type="email" placeholder="Seu email"/>
-                <input type="password" placeholder="Sua senha"/>
+                <input value={userName} onChange={e => setUserName(e.target.value)} type="text" placeholder="Nome de usuário"/>
+                <input value={email} onChange={e => setEmail(e.target.value)}  type="email" placeholder="Seu email"/>
+                <input value={pass} onChange={e => setPass(e.target.value)}  type="password" placeholder="Sua senha"/>
 
                 <div>
-                    <button type="button">Criar</button>
+                    <button onClick={handleSubmit} type="button">Criar</button>
                     <button onClick={()=>{navigate("/")}} id="cancelButton" type="button">Cancelar</button>
                 </div>
 
             </form>
+
+            {isOpen && <Popup
+                content={isLoading? <Loading/> :
+                    <SimpleMessage title={validAccount? customMessages.title.valid :
+                        customMessages.title.invalid}
+                    description={validAccount? customMessages.description.valid:
+                        customMessages.description.invalid}
+                /> }
+                bColor = {validAccount ? "#BBF247" : "yellow"}
+                handleClose={togglePopup}
+            />}
+
         </div>
     )
 }
--- frontend/src/pages/RequestLogin/index.jsx
+++ frontend/src/pages/RequestLogin/index.jsx
@@ -1,19 +1,25 @@
 import "./style.css"
 
 import { Link } from "react-router-dom"
+import LogoImg from "../../assets/logo.svg"
 
 export const RequestLogin = () => {
     return (
         <div className="RequestLogin">
+
+            <img src={LogoImg}/>
+
             <h1>
-                Olá, com o devindicator proporciona que qualquer usário possa 
+                Olá, o devindicator proporciona que qualquer usuário possa 
             indicar conteudos para outros usuarios. Mas para isso, é nessario 
-            que voçê faça login no sistema caso possua uma conta ou crie uma. 
-            Basta clicar no botão de logar ou criar uma conta 😁  
+            que voçê faça login no sistema caso possua uma conta, ou crie uma caso não tenha uma 😁
+              
             </h1>
 
-            <h2>não fez login? <Link to="/login" className="Link">Logar</Link> </h2>
-            <h2>Não possui uma conta? <Link to="/create-account" className="Link">Criar</Link> </h2>
+            <div>
+                <h2>não fez login? <Link to="/login" className="Link">Logar</Link> </h2>
+                <h2>não possui uma conta? <Link to="/create-account" className="Link">Criar</Link> </h2>
+            </div>
             
         </div>
     )
